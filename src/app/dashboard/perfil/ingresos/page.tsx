9cb247a9078cee9ea3144f72f3fd161188d83eb3
marcfbob <msbobzin@gmail.com>
2024-05-30 11:14:23 -0300

    agregué paginas de perfil

---
+++ src/app/dashboard/perfil/ingresos/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from "app/app/_components/ui/title";
+
+export default function Canchas {
+    return(
+        <div>
+            <Title>Ingresos</Title>
+        </div>
+    )
+}
--- src/app/dashboard/perfil/page.tsx
+++ src/app/dashboard/perfil/page.tsx
@@ -1,11 +1,20 @@
 import Navbar from "app/app/_components/Navbar"
+import { Title } from "app/app/_components/ui/title"
+import { Sidebar } from "lucide-react"
 
 export default function Home () {
 
     return(
         <div>
-        <h1>Perfil</h1>
+        <Title>Perfil</Title>
+        <Sidebar>
+        
+        </Sidebar>
+        <div>
+        <Title>Dashboard</Title>
+        </div>
 
         </div>
+
     )
 }
