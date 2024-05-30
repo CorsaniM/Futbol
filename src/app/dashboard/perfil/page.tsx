665229d131bca4ce5cdde1efd7a11b4c3d85a06a
Maximo <MaximoCorsani@gmail.com>
2024-05-30 11:21:06 -0300

    Add list

--- src/app/dashboard/perfil/page.tsx
+++ src/app/dashboard/perfil/page.tsx
@@ -1,13 +1,23 @@
-import Navbar from "app/app/_components/Navbar"
+import { List, ListTile } from "app/app/_components/ui/list";
 import { Title } from "app/app/_components/ui/title"
 import { Sidebar } from "lucide-react"
 
 export default function Home () {
+
+    const items = [
+        { es: "canchas", en: "Canchas" },
+        { es: "ingresos", en: "Ingresos" },
+        { es: "dashboard", en: "Dashboard" },
+        { es: "mensajes", en: "Mensajes" },
+    ];
+    
+
+
     return(
         <div>
         <Title>Perfil</Title>
         <Sidebar>
-        <div></div>
+
         </Sidebar>
         <div>
         <Title>Dashboard</Title>
@@ -17,3 +27,13 @@ export default function Home () {
 
     )
 }
+
+{/* <List>
+{items.map(({ es, en }) => (
+    <ListTile
+    key={es}
+    href={`/dashboard/faIKDivwt7Z8Gp-B5yFrv/maintenance/tables/${en}`}
+    title={en as React.ReactNode}
+    />
+))}
+</List> */}
