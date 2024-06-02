47361c1533d01211df4c0a684cb3ac38c902d548
marcfbob <msbobzin@gmail.com>
2024-06-02 20:33:18 -0300

    .

--- src/app/dashboard/perfil/page.tsx
+++ src/app/dashboard/perfil/page.tsx
@@ -4,16 +4,6 @@ import { Sidebar } from "lucide-react"
 import Link from "next/link";
 
 export default function Home () {
-
-    const lista = [
-        { es: "/dashboard/perfil/canchas", en: "Canchas" },
-        { es: "/dashboard/perfil/ingresos", en: "Ingresos" },
-        { es: "/dashboard/perfil/dashboard", en: "Dashboard" },
-        { es: "/dashboard/perfil/mensajes", en: "Mensajes" },
-    ];
-    
-
-
     return(
         <div>
         <Title>Perfil</Title>
@@ -21,12 +11,3 @@ export default function Home () {
     )
 }
 
-{/* <List>
-{items.map(({ es, en }) => (
-    <ListTile
-    key={es}
-    href={`/dashboard/faIKDivwt7Z8Gp-B5yFrv/maintenance/tables/${en}`}
-    title={en as React.ReactNode}
-    />
-))}
-</List> */}
