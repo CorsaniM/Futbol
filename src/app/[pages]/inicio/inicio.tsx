0c3f0a3ca0752aac7b0f670f7fd1ee2017b460df
Maximo <MaximoCorsani@gmail.com>
2024-06-04 09:36:23 -0300

    .

--- src/app/[pages]/inicio/inicio.tsx
+++ src/app/[pages]/inicio/inicio.tsx
@@ -8,7 +8,7 @@ export default function Inicio() {
         <div>
             <main className="flex min-h-screen bg-white">
             <div className=" mt-20 ml-20 h-40">
-                <h1>Queres subir tu complejo deportivo? <Link href="/registroalquiler" className="text-green-400">Ingresa aca</Link></h1>
+                <h1>Queres subir tu complejo deportivo? <Link href="/dashboard/registroalquiler" className="text-green-400">Ingresa aca</Link></h1>
             </div>
             <div className="flex items-center flex-col justify-center ">
             <div className="bg-slate-200 rounded-lg p-5 ">
--- src/app/dashboard/alquiler/page.tsx
+++ src/app/dashboard/alquiler/page.tsx
@@ -1,16 +1,7 @@
-import Navbar from "app/app/_components/Navbar";
-import { api } from "app/trpc/server";
-import Link from "next/link";
+
 import Alquilar from "./alquilar";
 
 export default function Alquiler () {
-
-
-
-    
-        
-    
-
 
     return(
         <div>
