93a78c9a573c6d424f3e87fca75a743fade224f3
marcfbob <msbobzin@gmail.com>
2024-06-05 01:11:53 -0300

    .

--- src/app/[pages]/inicio/inicio.tsx
+++ src/app/[pages]/inicio/inicio.tsx
@@ -10,11 +10,6 @@ export default function Inicio() {
                 <h1>¿Querés subir tu complejo deportivo?</h1>
                 <h2><Link href="/registroalquiler" className="text-green-600">Ingresa aca</Link></h2>
             <div>
-            
-            <div className="flex min-h-screen bg-white">
-                <div className=" mt-20 ml-20 h-40">
-                    <h1>Queres subir tu complejo deportivo? <Link href="/dashboard/registroalquiler" className="text-green-400">Ingresa aca</Link></h1>
-                </div>
             <div className="flex items-center flex-col justify-center text-2xl text-green-600 ">
                 <div className="w-80 rounded-full p-5 m-5 bg-slate-100 hover:bg-slate-200">
                     <Link href="/dashboard/deportes">Agregar un deporte</Link>
@@ -27,6 +22,8 @@ export default function Inicio() {
                 </div>
             </div>
         </div>
+        </div>
+    </div>
     </div>
     )
 }
